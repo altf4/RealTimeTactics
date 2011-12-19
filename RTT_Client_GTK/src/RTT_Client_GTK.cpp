4617002d596d5f89666ebdc2961c4bfaa20a739e
AltF4 <altf4@phx2600.org>
2011-12-18 22:46:19 -0700

    Updated Client_GTK to use gtkmm, and added callback
    - The button now actually prints something1

--- RTT_Client_GTK/src/RTT_Client_GTK.cpp
+++ RTT_Client_GTK/src/RTT_Client_GTK.cpp
@@ -5,53 +5,32 @@
 // Description : GTK Interface to the Client_Core
 //============================================================================
 
-#include "stdlib.h"
 #include <iostream>
-#include <gtk/gtk.h>
+#include <gtkmm.h>
 
 using namespace std;
+using namespace Gtk;
 
-G_MODULE_EXPORT
-void custom_server_click( GtkButton *button, gpointer data )
+void custom_server_click()
 {
-	exit(1);
+	cout << "SUCCESS!!\n";
 }
 
-int main( int argc, char **argv)
+extern "C" int main( int argc, char **argv)
 {
-	GtkBuilder *builder;
-	GtkWidget  *window;
-	GError     *error = NULL;
+	Main kit(argc, argv);
 
-	/* Init GTK+ */
-	gtk_init( &argc, &argv );
+	Window *window = NULL;
+	Button *button = NULL;
 
-	/* Create new GtkBuilder object */
-	builder = gtk_builder_new();
-	/* Load UI from file. If error occurs, report it and quit application.
-	* Replace "tut.glade" with your saved project. */
-	if( ! gtk_builder_add_from_file( builder, "UI/MainWindow.glade", &error ) )
-	{
-		g_warning( "%s", error->message );
-		g_free( error );
-		return( 1 );
-	}
+	Glib::RefPtr<Builder> builder = Builder::create_from_file("UI/MainWindow.glade");
+	builder->get_widget("window_main", window);
+	builder->get_widget("button_connect", button);
 
-	/* Get main window pointer from UI */
-	window = GTK_WIDGET( gtk_builder_get_object( builder, "window_main" ) );
+	button->signal_clicked().connect(sigc::ptr_fun(custom_server_click));
 
-	/* Connect signals */
-	gtk_builder_connect_signals( builder, NULL );
+	Main::run(*window);
 
-	/* Destroy builder, since we don't need it anymore */
-	g_object_unref( G_OBJECT( builder ) );
-
-	/* Show window. All other widgets are automatically shown by GtkBuilder */
-	gtk_widget_show( window );
-
-	/* Start main loop */
-	gtk_main();
-
-	return( 0 );
+	return EXIT_SUCCESS;
 }
 
