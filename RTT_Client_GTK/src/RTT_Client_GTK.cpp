dbcc74fc84aa0a8e28e66f012f1170a1862af238
AltF4 <altf4@phx2600.org>
2011-12-19 21:07:04 -0700

    Client_GTK: Welcome window has more flair
    - Text boxes and button for Custom Server Connect

--- RTT_Client_GTK/src/RTT_Client_GTK.cpp
+++ RTT_Client_GTK/src/RTT_Client_GTK.cpp
@@ -11,21 +11,27 @@
 using namespace std;
 using namespace Gtk;
 
+Window *window = NULL;
+Button *button = NULL;
+Statusbar *statusbar = NULL;
+Box *box_custom = NULL;
+
 void custom_server_click()
 {
-	cout << "SUCCESS!!\n";
+	statusbar->push("Set server settings, then hit Connect");
+	box_custom->set_visible(true);
+
 }
 
 extern "C" int main( int argc, char **argv)
 {
 	Main kit(argc, argv);
 
-	Window *window = NULL;
-	Button *button = NULL;
-
 	Glib::RefPtr<Builder> builder = Builder::create_from_file("UI/MainWindow.glade");
 	builder->get_widget("window_main", window);
-	builder->get_widget("button_connect", button);
+	builder->get_widget("button_custom", button);
+	builder->get_widget("status_main", statusbar);
+	builder->get_widget("box_custom", box_custom);
 
 	button->signal_clicked().connect(sigc::ptr_fun(custom_server_click));
 
