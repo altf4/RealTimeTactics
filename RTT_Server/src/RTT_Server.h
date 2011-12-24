832f16c5db45215c610f9cb2eb7083f882320350
AltF4 <altf4@phx2600.org>
2011-12-23 20:28:47 -0700

    Server: Removed unnecessary eqstr function
    - Was used in the old playerList char* hash table comparisons

--- RTT_Server/src/RTT_Server.h
+++ RTT_Server/src/RTT_Server.h
@@ -30,14 +30,6 @@ using tr1::hash;
 #define SERVER_VERSION_MINOR	0
 #define SERVER_VERSION_REV		1
 
-struct eqstr
-{
-  bool operator()(const char* s1, const char* s2) const
-  {
-    return (s1 == s2) || (s1 && s2 && strcmp(s1, s2) == 0);
-  }
-};
-
 struct eqint
 {
   bool operator()(int s1, int s2) const
