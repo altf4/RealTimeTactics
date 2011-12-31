11492558533240d9e7d764a37aaedb27ce89c27d
Nysomin <markpetro.mp@gmail.com>
2011-12-31 06:13:17 -0700

    Upstream merge

--- RTT_Client_GTK/Debug/src/RTT_Client_GTK.d
+++ RTT_Client_GTK/Debug/src/RTT_Client_GTK.d
@@ -4,8 +4,8 @@ src/RTT_Client_GTK.d: ../src/RTT_Client_GTK.cpp \
  ../../RTT_Common/src/Tile.h ../../RTT_Common/src/Unit.h \
  ../../RTT_Common/src/Team.h ../../RTT_Common/src/Player.h \
  ../../RTT_Common/src/Match.h ../../RTT_Common/src/Enums.h \
- ../../RTT_Common/src/Action.h \
- ../../RTT_Common/src/messages/LobbyMessage.h \
+ ../../RTT_Common/src/Action.h ../../RTT_Common/src/Map.h \
+ ../../RTT_Common/src/Map.h ../../RTT_Common/src/messages/LobbyMessage.h \
  ../../RTT_Common/src/messages/Message.h \
  ../../RTT_Common/src/messages/../Match.h \
  ../../RTT_Common/src/messages/ErrorMessage.h ../src/RTT_Client_GTK.h \
@@ -947,6 +947,10 @@ src/RTT_Client_GTK.d: ../src/RTT_Client_GTK.cpp \
 ../../RTT_Common/src/Enums.h:
 
 ../../RTT_Common/src/Action.h:
+
+../../RTT_Common/src/Map.h:
+
+../../RTT_Common/src/Map.h:
 
 ../../RTT_Common/src/messages/LobbyMessage.h:
 
--- RTT_Common/Debug/src/messages/LobbyMessage.d
+++ RTT_Common/Debug/src/messages/LobbyMessage.d
@@ -4,7 +4,7 @@ src/messages/LobbyMessage.d: ../src/messages/LobbyMessage.cpp \
  ../src/messages/../Tile.h ../src/messages/../Unit.h \
  ../src/messages/../Team.h ../src/messages/../Player.h \
  ../src/messages/../Match.h ../src/messages/../Enums.h \
- ../src/messages/../Action.h
+ ../src/messages/../Action.h ../src/messages/../Map.h
 
 ../src/messages/LobbyMessage.h:
 
@@ -27,3 +27,5 @@ src/messages/LobbyMessage.d: ../src/messages/LobbyMessage.cpp \
 ../src/messages/../Enums.h:
 
 ../src/messages/../Action.h:
+
+../src/messages/../Map.h:
