02667c150c67469431020409ecb39a316d15b8ac
AltF4 <altf4@phx2600.org>
2012-03-08 21:10:01 -0700

    Client_Core: Initial UI_Core GameEvent support
    - Not much more than a function declaration

--- RTT_Client_Core/src/GameEvents.cpp
+++ RTT_Client_Core/src/GameEvents.cpp
@@ -7,5 +7,30 @@
 //============================================================================
 
 #include "GameEvents.h"
+#include "messages/Message.h"
+#include "messages/GameMessage.h"
 
+#include <iostream>
 
+using namespace RTT;
+
+extern int callbackSocket;
+
+//Parent function of all Game Events
+//	This function listens on the callback socket for Game Events
+//	When an event is read, it is processed by calling the sub-functions below
+//	returns - true if the game is still running: IE: We should call this function again
+//		a return of false indicates that the game is over, and we should enter the MainLobby
+bool GameEvents::ProcessGameEvent()
+{
+	Message *event_message = Message::ReadMessage(callbackSocket);
+	if(event_message == NULL)
+	{
+		cerr << "ERROR: Message read from server failed. Did it die?\n";
+		return false;
+	}
+
+	//TODO: Handle the unit moved message!
+
+	return true;
+}
